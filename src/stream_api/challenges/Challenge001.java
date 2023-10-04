ade17dd3f861654db3b084e21c7ef06f0790e5a2
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-04 19:43:18 -0300

    feat: challenge001 completed

---
+++ src/stream_api/challenges/Challenge001.java
@@ -0,0 +1,12 @@
+package stream_api.challenges;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Challenge001 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        numbers.stream().sorted().forEachOrdered(System.out::println);
+    }
+}
