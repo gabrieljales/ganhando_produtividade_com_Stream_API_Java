b6c255c43d6bc5cabe72268760d435947e728948
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-10-05 11:00:36 -0300

    feat: challenge005 completed

---
+++ src/stream_api/challenges/Challenge005.java
@@ -0,0 +1,18 @@
+package stream_api.challenges;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Challenge005 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        double numbersBiggerThenFiveAverage = numbers.stream()
+                .filter(number -> number > 5) // Filtrando somente números maiores que 5
+                .mapToDouble(number -> number.doubleValue()) // Convertendo inteiros em ponto flutuante (double). Usando method reference, ficaria assim .mapToDouble(Integer::doubleValue)
+                .average() // Retorna um Optional, mais especificamente o OptionalDouble, contendo a média ou vazio (null)
+                .orElse(0.0); // Para tratar os casos em que o resultado é vazio (poderia também lançar uma exceção, com o .orElseThrow())
+
+        System.out.println(numbersBiggerThenFiveAverage);
+    }
+}
